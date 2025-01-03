3343abe3d0f53b0b91381536f846c16daedfebb3
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-01-03 01:01:09 +0100

    Creating the skeleton of test files

---
+++ SolarWatchTest/GeocodeProviderTest.cs
@@ -0,0 +1,16 @@
+namespace SolarWatchTest
+{
+    public class GeocodeProviderTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
