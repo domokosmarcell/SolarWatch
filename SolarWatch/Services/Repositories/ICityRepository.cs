71b6532fd9c6d17f6ca0c69f09518669b0b3978f
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-01-18 17:53:04 +0100

    Creating Repository interfaces

---
+++ SolarWatch/Services/Repositories/ICityRepository.cs
@@ -0,0 +1,10 @@
+﻿using SolarWatch.Models;
+
+namespace SolarWatch.Services.Repositories
+{
+    public interface ICityRepository
+    {
+        City GetByName(string city);
+        int Add(City city);
+    }
+}
---
+++ SolarWatch/Services/Repositories/ISolarTimeInfoRepository.cs
@@ -0,0 +1,10 @@
+﻿using SolarWatch.Models;
+
+namespace SolarWatch.Services.Repositories
+{
+    public interface ISolarTimeInfoRepository
+    {
+        SolarTimeInfo GetByCityAndDate(City city, DateOnly date);
+        int Add(SolarTimeInfo solarTimeInfo);
+    }
+}
