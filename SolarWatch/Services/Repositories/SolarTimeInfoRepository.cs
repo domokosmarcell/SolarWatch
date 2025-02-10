ecca5daf823740b4573477296880689535975017
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-02-10 18:54:41 +0100

    Creating SolarTimeInfoRepository

--- SolarWatch/Services/Repositories/SolarTimeInfoRepository.cs
+++ SolarWatch/Services/Repositories/SolarTimeInfoRepository.cs
@@ -1,17 +1,33 @@
 ﻿using SolarWatch.Models;
+using SolarWatch.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace SolarWatch.Services.Repositories
 {
     public class SolarTimeInfoRepository : ISolarTimeInfoRepository
     {
-        public Task Add(SolarTimeInfo solarTimeInfo)
+        private readonly SolarWatchContext _context;
+        public SolarTimeInfoRepository(SolarWatchContext solarWatchContext)
         {
-            throw new NotImplementedException();
+            _context = solarWatchContext;
+        }
+        public async Task<SolarTimeInfo> Add(SolarTimeInfo solarTimeInfo)
+        {
+            await _context.SolarTimeInfos.AddAsync(solarTimeInfo);
+            await _context.SaveChangesAsync();
+            return solarTimeInfo;
         }
 
-        public Task<SolarTimeInfo?> GetByCityAndDate(City city, DateOnly date)
+        public async Task<SolarTimeInfo?> GetByCityAndDate(City city, DateOnly date)
         {
-            throw new NotImplementedException();
+            return await _context.SolarTimeInfos.Include(x => x.City).FirstOrDefaultAsync(x => x.City.Id == city.Id && x.Date == date);
+        }
+
+        public async Task<SolarTimeInfo> Update(SolarTimeInfo solarTimeInfo)
+        {
+            _context.SolarTimeInfos.Update(solarTimeInfo);
+            await _context.SaveChangesAsync();
+            return solarTimeInfo; 
         }
     }
 }
