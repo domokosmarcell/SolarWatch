26c7f9bf79293b91d2516e9d9083b2644cd2f5ec
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-02-18 14:03:55 +0100

    Login feature implemented

--- SolarWatch/Services/Authentication/IAuthService.cs
+++ SolarWatch/Services/Authentication/IAuthService.cs
@@ -3,5 +3,6 @@
     public interface IAuthService
     {
         Task<AuthResult> RegisterAsync(string email, string username, string password);
+        Task<AuthResult> LoginAsync(string email, string password);
     }
 }
