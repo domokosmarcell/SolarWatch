6b2ab05e019431e70f051b826f43302cf5556cdd
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-03-07 15:47:39 +0100

    new migration added

---
+++ SolarWatch/Migrations/20250307141813_SunriseSunsetTimesAreNotNullAndRequired.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolarWatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class SunriseSunsetTimesAreNotNullAndRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Sunset",
+                table: "SolarTimeInfos",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0),
+                oldClrType: typeof(TimeOnly),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Sunrise",
+                table: "SolarTimeInfos",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0),
+                oldClrType: typeof(TimeOnly),
+                oldType: "time",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Sunset",
+                table: "SolarTimeInfos",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Sunrise",
+                table: "SolarTimeInfos",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+        }
+    }
+}
