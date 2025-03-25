e1bb768dbb28741bfefa33a2f22826b5b91556e1
Domokos Marcell <domokosmarcelldaniel@gmail.com>
2025-03-26 00:28:40 +0100

    Two tests for the SunriseController refactored

--- SolarWatchTest/SunriseControllerTest.cs
+++ SolarWatchTest/SunriseControllerTest.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SolarWatch.Controllers;
+using SolarWatch.Models;
 using SolarWatch.Services.ApiProviders;
 using SolarWatch.Services.JsonProcessors;
+using SolarWatch.Services.Repositories;
 
 namespace SolarWatchTest
 {
@@ -20,14 +22,14 @@ namespace SolarWatchTest
         private Mock<IGeocodeJsonProcessor> _geocodeJsonProcessorMock;
         private Mock<ISolarTimeProvider> _solarTimeProviderMock;
         private Mock<ISolarTimeJsonProcessor> _solarTimeJsonProcessorMock;
+        private Mock<ISolarTimeInfoRepository> _solarTimeInfoRepositoryMock;
+        private Mock<ICityRepository> _cityRepositoryMock;
         private SunriseController _controller;
+        private readonly string _cityInput = "Budapest,HU";
+        private readonly DateOnly _dateInput = new(2023, 12, 01);
+        private readonly string _tzidInput = "UTC";
         private readonly string _geocodeData = "[{}]";
         private readonly string _solarTimeData = "{}";
-        private readonly string _cityInput = "city,state,country";
-        private readonly DateOnly _dateInput = new DateOnly(2023, 1, 12);
-        private readonly string? _tzidInput = "UTC";
-        private readonly (float lat, float lon) _geocodes = (It.IsAny<float>(), It.IsAny<float>());
-        private readonly (TimeOnly sunrise, TimeOnly sunset) _solarTimes = (It.IsAny<TimeOnly>(), It.IsAny<TimeOnly>());
 
         [SetUp]
         public void SetUp()
@@ -37,14 +39,17 @@ namespace SolarWatchTest
             _geocodeJsonProcessorMock = new Mock<IGeocodeJsonProcessor>();
             _solarTimeProviderMock = new Mock<ISolarTimeProvider>();
             _solarTimeJsonProcessorMock = new Mock<ISolarTimeJsonProcessor>();
+            _solarTimeInfoRepositoryMock = new Mock<ISolarTimeInfoRepository>();
+            _cityRepositoryMock = new Mock<ICityRepository>();
             _controller = new SunriseController(_loggerMock.Object, _geocodeProviderMock.Object, _geocodeJsonProcessorMock.Object,
-                _solarTimeProviderMock.Object, _solarTimeJsonProcessorMock.Object);
+                _solarTimeProviderMock.Object, _solarTimeJsonProcessorMock.Object, _solarTimeInfoRepositoryMock.Object, _cityRepositoryMock.Object);
         }
 
         [Test]
-        public async Task GetSunriseTimeReturnsBadRequestIfGeocodeProviderFails()
+        public async Task GetSunriseTime_ReturnsBadRequest_IfGeocodeProviderFails()
         {
-            _geocodeProviderMock.Setup(x => x.GetGeocode(_cityInput)).ThrowsAsync(new Exception());
+            _cityRepositoryMock.Setup(x => x.GetByName(_cityInput)).ReturnsAsync((City)null);
+            _geocodeProviderMock.Setup(x => x.GetGeocode(_cityInput)).ThrowsAsync(new HttpRequestException());
 
             var result = await _controller.GetSunriseTime(_dateInput, _cityInput, _tzidInput);
 
@@ -52,18 +57,25 @@ namespace SolarWatchTest
         }
 
         [Test]
-        public async Task GetSunriseTimeReturnsBadRequestIfGeocodeJsonIsNotValid()
+        public async Task GetSunriseTime_ReturnsBadRequest_IfGeocodeJsonIsNotValid()
         {
-            _geocodeProviderMock.Setup(x => x.GetGeocode(_cityInput)).ReturnsAsync(_geocodeData);
-            _geocodeJsonProcessorMock.Setup(x => x.ProcessGeocodeInfo(_geocodeData, _cityInput)).Throws(new Exception());
+            var nonExistingCity = "fdgbg,US";
+            var errorMessage = "The city name and country code " +
+                    "(and where it is needed state name) that you provided is not valid or cannot be found " +
+                    "in the Geocoding API database!";
+            _cityRepositoryMock.Setup(x => x.GetByName(nonExistingCity)).ReturnsAsync((City)null);
+            _geocodeProviderMock.Setup(x => x.GetGeocode(nonExistingCity)).ReturnsAsync(_geocodeData);
+            _geocodeJsonProcessorMock.Setup(x => x.ProcessGeocodeInfo(_geocodeData, nonExistingCity)).Throws(new Exception(errorMessage));
 
-            var result = await _controller.GetSunriseTime(_dateInput, _cityInput, _tzidInput);
+            var result = await _controller.GetSunriseTime(_dateInput, nonExistingCity, _tzidInput);
 
             Assert.That(result.Result, Is.InstanceOf<BadRequestObjectResult>());
+            var badRequest = (BadRequestObjectResult)result.Result;
+            Assert.That(badRequest.Value, Is.EqualTo(errorMessage));
         }
 
         [Test]
-        public async Task GetSunriseTimeReturnsBadRequestIfSolarTimeProviderFails()
+        public async Task GetSunriseTime_ReturnsBadRequest_IfSolarTimeProviderFails()
         {
             _geocodeProviderMock.Setup(x => x.GetGeocode(_cityInput)).ReturnsAsync(_geocodeData);
             _geocodeJsonProcessorMock.Setup(x => x.ProcessGeocodeInfo(_geocodeData, _cityInput)).Returns(_geocodes);
